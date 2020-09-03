caa82f26bd6c0528d983083aa3da47bfe7e38ddc
Aniket Chaudhary <aniket@caringcompany.in>
2020-09-04 03:20:25 +0530

    added transition

--- src/components/hello-world.js
+++ src/components/hello-world.js
@@ -11,7 +11,31 @@ window.addEventListener("DOMContentLoaded", (event) => {
     iFramePopup.style.height = "460px";
     iFramePopup.style.width = "460px";
     iFramePopup.style.border = "0";
+    iFramePopup.style.opacity = 0;
+    iFramePopup.style.transform = "translateY(-1rem)";
+    iFramePopup.style.transition =
+      "opacity 150ms cubic - bezier(0.4, 0, 0.2, 1) 0ms, transform 161ms cubic - bezier(0.4, 0, 0.2, 1) 0ms";
   }
+
+  //   .date - picker - enter {
+  //     opacity: 0;
+  //     transform: translateY(-1rem);
+  //   }
+  // .date - picker - enter - active {
+  //     opacity: 1;
+  //     transition: opacity 150ms cubic - bezier(0.4, 0, 0.2, 1) 0ms,
+  //       transform 161ms cubic - bezier(0.4, 0, 0.2, 1) 0ms;
+  //     transform: translateY(0);
+  //   }
+  // .date - picker - exit {
+  //     opacity: 1;
+  //   }
+  // .date - picker - exit - active {
+  //     opacity: 0;
+  //     transform: translateY(-0.3rem);
+  //     transition: opacity 241ms cubic - bezier(0.4, 0, 0.2, 1) 0ms,
+  //       transform 161ms cubic - bezier(0.4, 0, 0.2, 1) 0ms;
+  //   }
 
   document.body.appendChild(iFramePopup);
 
@@ -22,6 +46,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
   if (getParentButtonId && iFramePopup) {
     getParentButtonId.addEventListener("click", function () {
       iFramePopup.style.display = "block";
+      iFramePopup.style.opacity = 1;
+      iFramePopup.style.transform = "translateY(0)";
       iFramePopup.style.left = `${
         getParentButtonId.getBoundingClientRect().left
       }px`;
