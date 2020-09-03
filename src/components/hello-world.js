fc0ac91dafdf597e8a2d266001d0bbec1573ad12
Aniket Chaudhary <aniket@caringcompany.in>
2020-09-04 01:18:35 +0530

    added document reader function

--- src/components/hello-world.js
+++ src/components/hello-world.js
@@ -1,39 +1,70 @@
-export default class {
-  static log() {
-    window.addEventListener("DOMContentLoaded", (event) => {
-      const iframeLink = "https://aniket.codes";
+window.addEventListener("DOMContentLoaded", (event) => {
+  const iframeLink = "https://aniket.codes";
 
-      //initial rendering of feedBack popup
-      let iFramePopup = document
-        .createElement("iframe")
-        .setAttribute("src", iframeLink);
-      if (iFramePopup) {
-        iFramePopup.style.display = "none";
-        iFramePopup.style.position = "absolute";
-      }
-      console.log(document);
-      console.log(document);
-      console.log("document");
-      console.log(iFramePopup);
-
-      //===========activate and display the popup================================
-      //clickedButton
-      const getParentButtonId = document.getElementById("getParentButtonId");
-      // console.log(getParentButtonId);
-
-      if (getParentButtonId && iFramePopup) {
-        iFramePopup.style.display = "block";
-        iFramePopup.style.left = `${
-          getParentButtonId.getBoundingClientRect().left
-        }px`;
-        iFramePopup.style.top = `${
-          getParentButtonId.getBoundingClientRect().top
-        }+${getParentButtonId.getBoundingClientRect().height}px`;
-      }
-    });
+  //initial rendering of feedBack popup
+  let iFramePopup = document
+    .createElement("iframe")
+    .setAttribute("src", iframeLink);
+  if (iFramePopup) {
+    iFramePopup.style.display = "none";
+    iFramePopup.style.position = "absolute";
   }
+  console.log(document);
+  console.log(document);
+  console.log("document");
+  console.log(iFramePopup);
 
-  static write() {
-    document.body.append("Hello World!");
+  //===========activate and display the popup================================
+  //clickedButton
+  const getParentButtonId = document.getElementById("getParentButtonId");
+  // console.log(getParentButtonId);
+
+  if (getParentButtonId && iFramePopup) {
+    iFramePopup.style.display = "block";
+    iFramePopup.style.left = `${
+      getParentButtonId.getBoundingClientRect().left
+    }px`;
+    iFramePopup.style.top = `${getParentButtonId.getBoundingClientRect().top}+${
+      getParentButtonId.getBoundingClientRect().height
+    }px`;
   }
-}
+});
+// export default class {
+//   static log() {
+//     window.addEventListener("DOMContentLoaded", (event) => {
+//       const iframeLink = "https://aniket.codes";
+
+//       //initial rendering of feedBack popup
+//       let iFramePopup = document
+//         .createElement("iframe")
+//         .setAttribute("src", iframeLink);
+//       if (iFramePopup) {
+//         iFramePopup.style.display = "none";
+//         iFramePopup.style.position = "absolute";
+//       }
+//       console.log(document);
+//       console.log(document);
+//       console.log("document");
+//       console.log(iFramePopup);
+
+//       //===========activate and display the popup================================
+//       //clickedButton
+//       const getParentButtonId = document.getElementById("getParentButtonId");
+//       // console.log(getParentButtonId);
+
+//       if (getParentButtonId && iFramePopup) {
+//         iFramePopup.style.display = "block";
+//         iFramePopup.style.left = `${
+//           getParentButtonId.getBoundingClientRect().left
+//         }px`;
+//         iFramePopup.style.top = `${
+//           getParentButtonId.getBoundingClientRect().top
+//         }+${getParentButtonId.getBoundingClientRect().height}px`;
+//       }
+//     });
+//   }
+
+//   static write() {
+//     document.body.append("Hello World!");
+//   }
+// }
