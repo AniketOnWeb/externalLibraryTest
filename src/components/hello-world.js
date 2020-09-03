af6c774197de84d92d74aefc137e4bc4f14d0a8f
Aniket Chaudhary <aniket@caringcompany.in>
2020-09-04 01:20:33 +0530

    added document reader function

--- src/components/hello-world.js
+++ src/components/hello-world.js
@@ -9,15 +9,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
     iFramePopup.style.display = "none";
     iFramePopup.style.position = "absolute";
   }
-  console.log(document);
-  console.log(document);
-  console.log("document");
-  console.log(iFramePopup);
 
   //===========activate and display the popup================================
   //clickedButton
   const getParentButtonId = document.getElementById("getParentButtonId");
-  // console.log(getParentButtonId);
+  console.log(getParentButtonId);
 
   if (getParentButtonId && iFramePopup) {
     iFramePopup.style.display = "block";
@@ -29,42 +25,3 @@ window.addEventListener("DOMContentLoaded", (event) => {
     }px`;
   }
 });
-// export default class {
-//   static log() {
-//     window.addEventListener("DOMContentLoaded", (event) => {
-//       const iframeLink = "https://aniket.codes";
-
-//       //initial rendering of feedBack popup
-//       let iFramePopup = document
-//         .createElement("iframe")
-//         .setAttribute("src", iframeLink);
-//       if (iFramePopup) {
-//         iFramePopup.style.display = "none";
-//         iFramePopup.style.position = "absolute";
-//       }
-//       console.log(document);
-//       console.log(document);
-//       console.log("document");
-//       console.log(iFramePopup);
-
-//       //===========activate and display the popup================================
-//       //clickedButton
-//       const getParentButtonId = document.getElementById("getParentButtonId");
-//       // console.log(getParentButtonId);
-
-//       if (getParentButtonId && iFramePopup) {
-//         iFramePopup.style.display = "block";
-//         iFramePopup.style.left = `${
-//           getParentButtonId.getBoundingClientRect().left
-//         }px`;
-//         iFramePopup.style.top = `${
-//           getParentButtonId.getBoundingClientRect().top
-//         }+${getParentButtonId.getBoundingClientRect().height}px`;
-//       }
-//     });
-//   }
-
-//   static write() {
-//     document.body.append("Hello World!");
-//   }
-// }
