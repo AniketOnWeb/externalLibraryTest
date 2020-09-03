da13021a22d06aec96ccebd3419d95e243f89484
Aniket Chaudhary <aniket@caringcompany.in>
2020-09-04 01:06:47 +0530

    added document reader function

--- src/components/hello-world.js
+++ src/components/hello-world.js
@@ -1,11 +1,34 @@
 export default class {
-
   static log() {
-    console.log('Hello world!')
-  }
-  
-  static write() {
-    document.body.append('Hello World!')
-  }
+    (function () {
+      window.addEventListener("DOMContentLoaded", (event) => {
+        const iframeLink = "https://aniket.codes";
 
+        //initial rendering of feedBack popup
+        let iFramePopup = document
+          .createElement("iframe")
+          .setAttribute("src", iframeLink);
+        if (iFramePopup) {
+          iFramePopup.style.display = "none";
+          iFramePopup.style.position = "absolute";
+        }
+        console.log(document);
+
+        //===========activate and display the popup================================
+        //clickedButton
+        const getParentButtonId = document.getElementById("getParentButtonId");
+        // console.log(getParentButtonId);
+
+        if (getParentButtonId && iFramePopup) {
+          iFramePopup.style.display = "block";
+          iFramePopup.style.left = `${
+            getParentButtonId.getBoundingClientRect().left
+          }px`;
+          iFramePopup.style.top = `${
+            getParentButtonId.getBoundingClientRect().top
+          }+${getParentButtonId.getBoundingClientRect().height}px`;
+        }
+      });
+    })();
+  }
 }
