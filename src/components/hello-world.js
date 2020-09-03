54122f0402a69d0f3a545e31e9590a24c0e9fd9c
Aniket Chaudhary <aniket@caringcompany.in>
2020-09-04 01:12:37 +0530

    added document reader function

--- src/components/hello-world.js
+++ src/components/hello-world.js
@@ -1,34 +1,35 @@
 export default class {
   static log() {
-    (function () {
-      window.addEventListener("DOMContentLoaded", (event) => {
-        const iframeLink = "https://aniket.codes";
+    window.addEventListener("DOMContentLoaded", (event) => {
+      const iframeLink = "https://aniket.codes";
 
-        //initial rendering of feedBack popup
-        let iFramePopup = document
-          .createElement("iframe")
-          .setAttribute("src", iframeLink);
-        if (iFramePopup) {
-          iFramePopup.style.display = "none";
-          iFramePopup.style.position = "absolute";
-        }
-        console.log(document);
+      //initial rendering of feedBack popup
+      let iFramePopup = document
+        .createElement("iframe")
+        .setAttribute("src", iframeLink);
+      if (iFramePopup) {
+        iFramePopup.style.display = "none";
+        iFramePopup.style.position = "absolute";
+      }
+      console.log(document);
+      console.log(document);
+      console.log("document");
+      console.log(iFramePopup);
 
-        //===========activate and display the popup================================
-        //clickedButton
-        const getParentButtonId = document.getElementById("getParentButtonId");
-        // console.log(getParentButtonId);
+      //===========activate and display the popup================================
+      //clickedButton
+      const getParentButtonId = document.getElementById("getParentButtonId");
+      // console.log(getParentButtonId);
 
-        if (getParentButtonId && iFramePopup) {
-          iFramePopup.style.display = "block";
-          iFramePopup.style.left = `${
-            getParentButtonId.getBoundingClientRect().left
-          }px`;
-          iFramePopup.style.top = `${
-            getParentButtonId.getBoundingClientRect().top
-          }+${getParentButtonId.getBoundingClientRect().height}px`;
-        }
-      });
-    })();
+      if (getParentButtonId && iFramePopup) {
+        iFramePopup.style.display = "block";
+        iFramePopup.style.left = `${
+          getParentButtonId.getBoundingClientRect().left
+        }px`;
+        iFramePopup.style.top = `${
+          getParentButtonId.getBoundingClientRect().top
+        }+${getParentButtonId.getBoundingClientRect().height}px`;
+      }
+    });
   }
 }
